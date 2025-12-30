bf1804a3030645e6f29f3696d66177e7749dbb96
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-30 22:50:34 +0530

    Add react-native-worklets and react-native-reanimated with Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+  /** @type {import('react-native-worklets/plugin').PluginOptions} */
+  const workletsPluginOptions = {}
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+      ...
+      ['react-native-worklets/plugin', workletsPluginOptions],
+    ],
 };
