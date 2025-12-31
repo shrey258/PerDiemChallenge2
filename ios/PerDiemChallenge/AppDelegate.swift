0ed506745d5dc69bda7cd04975b1cf561de192f7
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-31 12:30:33 +0530

    Add Firebase configuration for iOS and Android platforms

--- ios/PerDiemChallenge/AppDelegate.swift
+++ ios/PerDiemChallenge/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,11 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    FirebaseApp.configure()
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
 
-    reactNativeDelegate = delegate
+    reactNativeDelegate = delegate  
     reactNativeFactory = factory
 
     window = UIWindow(frame: UIScreen.main.bounds)
